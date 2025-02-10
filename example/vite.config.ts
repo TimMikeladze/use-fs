697d66a67ff2cd0b59da92a1a904f31007de06c9
Tim Mikeladze <tim.mikeladze@gmail.com>
2025-02-09 22:32:00 -0800

    fix: vite config

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -7,10 +7,10 @@ export default defineConfig({
 	plugins: [tailwindcss(), react()],
 	define: {
 		// biome-ignore lint/style/useNamingConvention: <explanation>
-		__APP_ENV__: process.env.VITE_VERCEL_ENV,
+		__APP_ENV__: JSON.stringify(process.env.VITE_VERCEL_ENV),
 		// biome-ignore lint/style/useNamingConvention: <explanation>
-		VITE_UMAMI_WEBSITE_ID: process.env.VITE_UMAMI_WEBSITE_ID,
+		VITE_UMAMI_WEBSITE_ID: JSON.stringify(process.env.VITE_UMAMI_WEBSITE_ID),
 		// biome-ignore lint/style/useNamingConvention: <explanation>
-		VITE_UMAMI_URL: process.env.VITE_UMAMI_URL,
+		VITE_UMAMI_URL: JSON.stringify(process.env.VITE_UMAMI_URL),
 	},
 });
